d2924317a44a65d63780045545fc2064a73626ea
C3NZ <cenz@cenz.io>
2019-08-03 14:21:39 -0700

    [add] axios as a dependency and setup the rest of the project

---
+++ src/controllers/weather.js
@@ -0,0 +1,10 @@
+const weather = require("openweather-apis");
+
+const processQueryStrings = (queryString) => {
+  
+}
+
+module.exports.getWeatherByCity = (req, res) => {};
+module.exports.getWeatherByZipCode = (req, res) => {};
+module.exports.getWeatherByCoordinates = (req, res) => {};
+module.exports.
