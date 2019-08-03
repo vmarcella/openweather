6efe619d42ce9a24188ad0141ed2b42c80214388
C3NZ <cenz@cenz.io>
2019-08-03 15:07:25 -0700

    [add] dotenv for environment variables, update the controller for handling getting the weather for a city, and then create a library for openweather

--- src/controllers/weather.js
+++ src/controllers/weather.js
@@ -1,10 +1,20 @@
-const weather = require("openweather-apis");
+const weather = require("../lib/weather");
 
-const processQueryStrings = (queryString) => {
-  
-}
+const processQueryStrings = (queryString) => {};
 
-module.exports.getWeatherByCity = (req, res) => {};
+module.exports.getWeatherByCity = async (req, res) => {
+  try {
+    if (!req.query.city) {
+      res.json({ status: "FAILED", msg: "City not provided as a query param" });
+    }
+    await weather.getWeatherByName(req.query.city);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      status: "INTERNAL ERROR",
+      msg: "Endpoint could not be accessed",
+    });
+  }
+};
 module.exports.getWeatherByZipCode = (req, res) => {};
 module.exports.getWeatherByCoordinates = (req, res) => {};
-module.exports.
--- src/lib/weather.js
+++ src/lib/weather.js
@@ -1,1 +1,17 @@
 const axios = require("axios");
+
+const appID = process.env.OPENWEATHER_KEY;
+console.log(appID);
+const ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?APPID=${appID}`;
+
+module.exports.getWeatherByName = async (cityName) => {
+  const query = `&q=${cityName}`;
+  const response = await axios.get(ENDPOINT + query);
+  console.log(response);
+};
+
+module.exports.getWeatherById = async (cityID) => {
+  const query = `&id=${cityID}`;
+  const response = await axios.get(ENDPOINT + query);
+  console.log(response);
+};
