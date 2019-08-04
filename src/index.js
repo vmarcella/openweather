cc7203d576e41575f0124ce8f4c812a8847ba3bb
C3NZ <cenz@cenz.io>
2019-08-03 22:27:29 -0700

    [add] a mongoose connection to the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const { apiRouter } = require("./routers/index");
 
+// Port and mongoose database connection
 const port = 3000;
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+
 const app = express();
 
 app.use(bodyParser.json());
