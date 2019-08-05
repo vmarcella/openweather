74f751b3869eaab0f3b0ab7b794428d474869098
C3NZ <cenz@cenz.io>
2019-08-04 18:47:45 -0700

    [fix] the route name for getting a mood for cities

--- src/routers/mood.js
+++ src/routers/mood.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
 const controller = require("../controllers/mood");
 
-// Routes and controllers for the 
+// Routes and controllers for the
 router.get("/", controller.getMoods);
 router.get("/:moodId", controller.getMood);
-router.get("/city", controller.getMoodByCity);
+router.get("/city", controller.getMoodsByCity);
 router.post("/", controller.postMood);
 
 module.exports = router;
