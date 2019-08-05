d1c5df8937510b0dc041d8a52de3e6222b012fb9
C3NZ <cenz@cenz.io>
2019-08-04 18:47:28 -0700

    [add] finishing touches to posting a mood

--- src/controllers/mood.js
+++ src/controllers/mood.js
@@ -1,5 +1,5 @@
 const Mood = require("../models/mood");
-
+const weather = require("../lib/weather");
 /**
  * @function
  * @desc Get all the moods ever stored on the server
@@ -20,14 +20,20 @@ module.exports.getMoods = async (req, res) => {
 
 module.exports.getMood = async (req, res) => {
   try {
-    const mood = await Mood.findById(req.params.id);
+    const mood = await Mood.findById(req.param.id);
     return res.json({ status: "OK", mood });
   } catch (err) {
     return res.status(500).json({ status: "FAILED", msg: err.message });
   }
 };
 
-module.exports.getMoodByCity = async (req, res) => {
+module.exports.getMoodsByCity = async (req, res) => {
+  if (!req.query.city) {
+    return res.status(422).json({
+      status: "FAILED",
+      msg: "You didn't provide the query string for the city",
+    });
+  }
   try {
     const moods = await Mood.find({ city: req.params.city });
     return res.json({ status: "OK", moods });
@@ -37,10 +43,32 @@ module.exports.getMoodByCity = async (req, res) => {
 };
 
 module.exports.postMood = async (req, res) => {
+  // user entered
+  if (!req.query.city) {
+    return res.status(422).json({
+      status: "FAILED",
+      msg: "You didn't provide the query string for the city",
+    });
+  }
+
+  // User entered incorrect data
+  if (!req.body.mood || !(req.body.mood instanceof String)) {
+    return res.status(403).json({
+      status: "FAILED",
+      msg:
+        "You either didn't provide your mood or provided it as something other than a string",
+    });
+  }
+
   try {
-    const createdMood = await Mood.create(req.body);
+    const weatherData = weather.getWeatherByName(req.query.city);
+
+    const mood = { mood: req.body.mood, weather: weatherData };
+    const createdMood = await Mood.create(mood);
     return res.json({ status: "OK", createdMood });
   } catch (err) {
-    return res.status(500).json({ status: "FAILED", msg: err.message });
+    return res
+      .status(err.status || 500)
+      .json({ status: "FAILED", msg: err.message });
   }
 };
